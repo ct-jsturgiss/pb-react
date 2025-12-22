581f03bc1f78d87d3b2f5579ccd5e99ae0be6ff3
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-22 15:56:57 -0700

    Added ui constants file. Added main menu types file.

---
+++ app/core/ui.constants.ts
@@ -0,0 +1,10 @@
+import type { AppMenuItem } from "./main-menu/types/main-menu.types";
+import {  } from "@mui/material/Icon"
+
+export const MainMenuItems = {
+    ivLookup: {
+        id: "iv-lookup",
+        label: "Inventory Lookup",
+        icon: "",
+    } as const as AppMenuItem
+}
