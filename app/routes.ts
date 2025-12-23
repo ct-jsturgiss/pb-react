ea60ca2ff41756e552c2920d1b5cbf38ce11955e
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-23 09:39:14 -0700

    Renamed iv lookup page file and updated route.

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route("iv-lookup", "routes/iv-lookup/iv-lookup.tsx")
+    route("iv-lookup", "routes/iv-lookup/iv-lookup.page.tsx")
 ] satisfies RouteConfig;
