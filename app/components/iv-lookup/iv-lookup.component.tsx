47034a0bb7db61e022fb80f2d06d2e65a51f7fcd
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-24 10:20:37 -0700

    Updated layout for iv lookup.

--- app/components/iv-lookup/iv-lookup.component.tsx
+++ app/components/iv-lookup/iv-lookup.component.tsx
@@ -1,30 +1,37 @@
-import { Button, Grid, TextInput, Text, Stack, Group } from "@mantine/core";
+import { Button, Grid, TextInput, Text, Stack, Group, Card, Paper } from "@mantine/core";
 import { IconX } from "@tabler/icons-react";
+import { DataTable } from "mantine-datatable";
 
 export default function IvLookup() {
 
     function buildLayout() {
         return (
-            <Grid className="pb page-host">
+            <Grid grow className="pb page-host" classNames={{inner: "pb h-100"}}>
                 <Grid.Col span={6}>
                     <Stack className="pb h-100">
                         <Text className="pb page-header">Inventory Lookup</Text>
                         <Grid grow>
-                            <Grid.Col span={{base: 10}}>
+                            <Grid.Col span={8}>
                                 <TextInput label="Search" size="sm"></TextInput>
                             </Grid.Col>
-                            <Grid.Col span={1} style={{alignContent: "end", maxWidth: "4.25rem"}}>
-                                <Button variant="contained">
+                            <Grid.Col span={"content"} style={{alignContent: "end", maxWidth: "fit-content"}}>
+                                <Button variant="contained" style={{maxWidth: "4.5rem"}}>
                                     <IconX></IconX>
                                 </Button>
                             </Grid.Col>
-                            <Grid.Col span={1} style={{alignContent: "end", maxWidth: "4.25rem"}}>
+                            <Grid.Col span={"content"} style={{alignContent: "end", maxWidth: "fit-content"}}>
                                 <Button variant="contained">View</Button>
                             </Grid.Col>
                         </Grid>
+                        <Paper shadow="md" radius={"md"} withBorder className="pb d-flex flex-fill flex-column">
+                            <DataTable
+                                className="pb m-2"
+                                columns={[{accessor: "id", title: "Test Col 1"}]}
+                                records={[{id: "Row 1"},{id: "Row 2"},{id: "Row 3"}]}
+                            />
+                        </Paper>
                     </Stack>
                     <Grid>
-                        
                     </Grid>
                 </Grid.Col>
                 <Grid.Col span={6}>
