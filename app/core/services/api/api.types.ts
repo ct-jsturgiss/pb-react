68c18408f2bff31304a0b40accc05b927b5420a7
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-29 09:47:46 -0700

    Consolidated interface files to types.

--- app/core/services/api/api.types.ts
+++ app/core/services/api/api.types.ts
@@ -1,8 +1,44 @@
-import type { ApiRequestResult } from "./api.interfaces";
-
 /**
  * String enum for type of API change request being submitted, corresponding to an HTTP method.
  */
 export type ApiChangeMethod = "PUT"|"PATCH"|"DELETE";
 
 export type ApiResultHandler = (response:ApiRequestResult) => void;
+
+export interface ApiRequestError {
+    msg:string;
+    code:number;
+}
+
+export interface QueryData<T> {
+    response:ApiRequestResult;
+    records:T[];
+}
+
+export interface ApiQueryRecords {
+    offset?:number;
+    pageSize?:number;
+    count:number;
+    records:any[];
+}
+
+export interface ApiResultError {
+    errorCode:number;
+    errorMessage:string;
+    recordKeys:any;
+}
+
+export interface ApiRequestResult {
+    isSuccess:boolean;
+    isError:boolean;
+    isFatal?:boolean;
+    message?:string[];
+    errors?:ApiResultError[]; // TODO: Change to errors object interface.
+    data?:ApiQueryRecords;
+    stateCode?:ApiRequestError
+}
+
+export interface ApiQueryBody {
+    "page_size"?:number;
+    "offset"?:number;
+}
