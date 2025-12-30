470376bd01459cbf2708fbbe230579ef9467ddb6
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-30 14:44:16 -0700

    Broke out iv lookup page and components. Moved state to iv lookup page.

--- app/components/iv-lookup/iv-lookup.component.tsx
+++ app/components/iv-lookup/iv-lookup.component.tsx
@@ -1,13 +1,20 @@
+"use client";
+
 // Mantine UI
-import { Button, Grid, TextInput, Text, Stack, Group, Card, Paper } from "@mantine/core";
-import { DataTable, type DataTableColumn } from "mantine-datatable";
+import { Button, Grid, Text, Stack } from "@mantine/core";
+import { type DataTableColumn } from "mantine-datatable";
 
 // Tabler Icons
 import { IconX } from "@tabler/icons-react";
 
 // PB
 import { useIvLookupStore, type IvLookupRecord } from "./iv-lookup.funcs";
+import { SearchBar } from "../search-bar/search-bar.component";
+import { RecordTable } from "../record-table/record-table.component";
+import type { IvLookupProps } from "./iv-lookup.types";
 
+// Constants
+const pageSize:number = 25;
 
 // Columns
 const columns:DataTableColumn[] = [
@@ -25,42 +32,43 @@ const columns:DataTableColumn[] = [
     }
 ];
 
-export default function IvLookup() {
+export default function IvLookup(props:IvLookupProps) {
+
+    // Props
+    const { isLoading } = props;
 
     // State
-    const view:IvLookupRecord[] = useIvLookupStore((state) => state.lookupsView);
+    const setSearch = useIvLookupStore(state => state.setSearchFilter);
+    const view:IvLookupRecord[] = useIvLookupStore(state => state.lookupsView);
+
+    function handleSearchChanged(value?:string) {
+        const v = value ?? null;
+        setSearch(v);
+    }
 
     function buildLayout() {
 
         return (
-            <Grid grow className="pb page-host" classNames={{inner: "pb h-100"}}>
-                <Grid.Col span={6}>
-                    <Stack className="pb h-100">
+            <Grid grow className="pb page-host" classNames={{inner: "pb h-100 min-h-100"}}>
+                <Grid.Col span={6} className="pb grid-column">
+                    <Stack className="pb h-100 min-h-100">
                         <Text className="pb page-header">Inventory Lookup</Text>
-                        <Grid grow>
-                            <Grid.Col span={8}>
-                                <TextInput label="Search" size="sm"></TextInput>
-                            </Grid.Col>
-                            <Grid.Col span={"content"} style={{alignContent: "end", maxWidth: "fit-content"}}>
-                                <Button variant="contained" style={{maxWidth: "4.5rem"}}>
-                                    <IconX></IconX>
-                                </Button>
-                            </Grid.Col>
-                            <Grid.Col span={"content"} style={{alignContent: "end", maxWidth: "fit-content"}}>
-                                <Button variant="contained">View</Button>
-                            </Grid.Col>
-                        </Grid>
-                        <Paper shadow="md" radius={"md"} withBorder className="pb d-flex flex-fill flex-column">
-                            <DataTable
-                                className="pb m-2"
-                                key={"id"}
-                                columns={columns}
-                                records={view}
-                            />
-                        </Paper>
+                        <SearchBar 
+                            label="Search" 
+                            placeholder="Enter search criteria..." 
+                            onTextChanged={(v) => handleSearchChanged(v)}>
+                            <Button variant="contained" style={{maxWidth: "4.5rem"}} className="pb mx-1">
+                                <IconX></IconX>
+                            </Button>
+                            <Button variant="contained">View</Button>
+                        </SearchBar>
+                        <RecordTable
+                            recordKey="id"
+                            columns={columns}
+                            recordSource={view}
+                            isLoading={isLoading ?? false}
+                        />
                     </Stack>
-                    <Grid>
-                    </Grid>
                 </Grid.Col>
                 <Grid.Col span={6}>
 
--- app/routes/iv-lookup/iv-lookup.page.tsx
+++ app/routes/iv-lookup/iv-lookup.page.tsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from "react";
+import { IvLookupAdapter } from "~/components/iv-lookup/iv-lookup.adapters";
 import IvLookup from "~/components/iv-lookup/iv-lookup.component";
+import { useIvLookupStore } from "~/components/iv-lookup/iv-lookup.funcs";
+import { useQueryIvLookups } from "~/components/iv-lookup/iv-lookup.hooks";
 
 export default function IvLookupPage() {
 
+    // Query
+    const ivLookupCacheQuery = useQueryIvLookups();
+    const setLookups = useIvLookupStore(state => state.setLookups);
+
+    // State
+    const [isLoadingRecords, setIsLoadingRecords] = useState<boolean>(true);
+
+    useEffect(() => {
+        
+        if(ivLookupCacheQuery.isPending) {
+            if(!isLoadingRecords) {
+                setIsLoadingRecords(true);
+            }
+        } else {
+            setIsLoadingRecords(false);
+            if(ivLookupCacheQuery.isError) {
+                // TODO: Do something
+            } else {
+                const queryResult = ivLookupCacheQuery.data;
+                const records = queryResult?.data?.records.map(r => IvLookupAdapter.fromJson(r)) ?? [];
+                setLookups(records);
+            }
+        }
+
+    }, [ivLookupCacheQuery.status]);
+
     return (
-        <IvLookup></IvLookup>
+        <IvLookup isLoading={isLoadingRecords}/>
     )
 
 }
