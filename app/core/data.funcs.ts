8f888fe63ae02cd6a9ce669a56f19ad169050df0
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2026-01-02 18:38:58 -0700

    Added comments and new getDistinctBy func for arrays.

--- app/core/data.funcs.ts
+++ app/core/data.funcs.ts
@@ -12,6 +12,10 @@ export const createRecordWithId = (id:number|null = null) => {
     }
 };
 
+/**
+ * Initializes a new search chain object for the specified type.
+ * @returns 
+ */
 export const createSearchChain = <T>() => {
     return {
         lastPattern: "",
@@ -19,10 +23,36 @@ export const createSearchChain = <T>() => {
     } as SearchChain<T>;
 }
 
+/**
+ * Attempts to detect an object as an implementation of {@link RecordView} and return it.
+ * @param record A record that should implement {@link RecordView}
+ * @returns The record as {@link RecordView} or undefined.
+ */
 export const asRecordView = (record:Record<string,unknown>):RecordView|undefined => {
     if("viewState" in record) {
         return record as unknown as RecordView;
     }
 
     return undefined;
+}
+
+/**
+ * Gets a distinct list of objects by the specified property keys.
+ * @param records Records to get a distinct array of.
+ * @param keys String names of the keys to use as comparison keys.
+ * @returns A distinct array of objects.
+ */
+export const getDistinctBy = <T>(records:T[], keys:[keyof T]|string[]) => {
+    const keySet = new Set();
+    const distinct:T[] = [];
+    for (const record of records) {
+        const len = keySet.size;
+        const key = keys.map(k => (record as any)[k]).join("~");
+        keySet.add(key);
+        if(len < keySet.size) {
+            distinct.push(record);
+        }
+    }
+
+    return distinct;
 }
